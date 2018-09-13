f4b025d2a603e570795b6594fc795f55d5036296
Abhishek Kumar <abhishek3433kumar@gmail.com>
2018-09-13 00:06:34 -0500

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -94,11 +94,47 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 
-    
+
     it('should return 404 for non-object ids', (done) => {
         request(app)
             .get(`/todos/test123`)
             .expect(404)
             .end(done);
     })
+});
+
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        let hexId = mockTodos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist;
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return a 404 if object id not valid', (done) => {
+        request(app)
+            .delete(`/todos/test123`)
+            .expect(404)
+            .end(done);
+    });
 });
